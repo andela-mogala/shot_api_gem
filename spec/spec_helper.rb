e971e56027a43bf6e92ce6a26a728d3e7deebdfe
Michael Ogala <michael.ogala@andela.com>
2016-11-07 13:23:23 +0100

    setup simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'shot_api_gem'
 require 'faker'
-require "codeclimate-test-reporter"
-CodeClimate::TestReporter.start
+require "simplecov"
+SimpleCov.start
